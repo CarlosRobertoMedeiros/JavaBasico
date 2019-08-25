76ddece583a921d63a95974c8e9a792e43e1d294
CarlosRobertoMedeiros <carlosmedeiroslima@gmail.com>
2019-08-25 13:09:25 -0300

    Revisão Livro AlgaWorks Tipos Primitivos

--- exemplos/src/Aluno.java
+++ exemplos/src/Aluno.java
@@ -1,7 +1,26 @@
 public class Aluno {
     public static void main(String[] args) {
+        imprimindoCaracteresdeEscapes();
+        tiposDeVariaveis();
+    }
+
+    private static void imprimindoCaracteresdeEscapes(){
+        System.out.println("Olá \n Mundo "); //Quebrando Linhas
+        System.out.println("Olá \"Mundo\" "); //Imprimindo as Aspas duplas
         System.out.println("Valor X");
+    }
+    private static void tiposDeVariaveis(){
+//        Não pode ser palavra reservada
+//        Não pode começar com número
+//        Não pode XXXX
+
+    }
+
+    public static void tiposPirmitivos(){
+        //boolean,char,byte,short,int,long,float,double
+
     }
 
 
 }
+
